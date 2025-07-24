67abac704f5394b40d3bbcc5ef437ed9a167004d
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2025-07-24 15:15:17 -0300

    fix: change name of Jefferson on team section

--- components/layout/sections/team.tsx
+++ components/layout/sections/team.tsx
@@ -80,7 +80,7 @@ export const TeamSection = () => {
       imageUrl: profile4,
       qrCodeWhatsApp: qrCode3,
       firstName: "Jefferson",
-      lastName: "Guedes",
+      lastName: "Couto",
       positions: ["Diretor de Marketing", "Promoter"],
       socialNetworks: [
         {
