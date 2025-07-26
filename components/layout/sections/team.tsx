fe0f47f6c6d5df7ca496304fa1b18820bfe4630f
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2025-07-25 23:07:11 -0300

    fix: team section responsive names

--- components/layout/sections/team.tsx
+++ components/layout/sections/team.tsx
@@ -151,7 +151,7 @@ export const TeamSection = () => {
         </h2>
       </div>
 
-      <div className="grid max-sm:grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
         {teamList.map(
           (
             {
